a3c294119b39bd4007aad362449c3ba70fb5201b
Lewis Uzoma <boblewisu@gmail.com>
2025-05-05 09:51:43 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'searchable',
+  webDir: 'www'
+};
+
+export default config;
